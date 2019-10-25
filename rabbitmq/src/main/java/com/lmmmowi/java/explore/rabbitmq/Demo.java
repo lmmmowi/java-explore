3ea27dd6a4b09f54a681bc6cec29d359ba25ad4b
lmmmowi <lmmmowi@163.com>
2019-10-25 08:59:04 +0800

    添加rabbitmq示例

---
+++ rabbitmq/src/main/java/com/lmmmowi/java/explore/rabbitmq/Demo.java
@@ -0,0 +1,76 @@
+package com.lmmmowi.java.explore.rabbitmq;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Consumer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @Author: 11102942
+ * @Date: 2019/10/25
+ * @Description:
+ */
+public class Demo {
+
+    private static final String QUEUE_NAME = "test_queue";
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    public static void main(String[] args) {
+        new Demo().run();
+    }
+
+    public void run() {
+        Connection connection = this.getConnection();
+        this.publish(connection, "Hello World");
+        this.consume(connection);
+        this.closeConnection(connection);
+    }
+
+    private void publish(Connection connection, String message) {
+        try (Channel channel = connection.createChannel()) {
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+            logger.info("发布了消息");
+        } catch (IOException | TimeoutException e) {
+            logger.error(e.getMessage(), e);
+        }
+    }
+
+    private void consume(Connection connection) {
+        try (Channel channel = connection.createChannel()) {
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            Consumer consumer = new DemoConsumer(channel);
+            channel.basicConsume(QUEUE_NAME, false, consumer);
+        } catch (IOException | TimeoutException e) {
+            logger.error(e.getMessage(), e);
+        }
+    }
+
+    private Connection getConnection() {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("localhost");
+        connectionFactory.setPort(5672);
+        connectionFactory.setUsername("guest");
+        connectionFactory.setPassword("guest");
+
+        try {
+            return connectionFactory.newConnection();
+        } catch (IOException | TimeoutException e) {
+            throw new IllegalStateException("fail to build a rabbitmq connection", e);
+        }
+    }
+
+    private void closeConnection(Connection connection) {
+        try {
+            connection.close();
+        } catch (IOException e) {
+            logger.error(e.getMessage(), e);
+        }
+    }
+}
---
+++ rabbitmq/src/main/java/com/lmmmowi/java/explore/rabbitmq/DemoConsumer.java
@@ -0,0 +1,32 @@
+package com.lmmmowi.java.explore.rabbitmq;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @Author: 11102942
+ * @Date: 2019/10/25
+ * @Description:
+ */
+public class DemoConsumer extends DefaultConsumer {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    public DemoConsumer(Channel channel) {
+        super(channel);
+    }
+
+    @Override
+    public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+        super.handleDelivery(consumerTag, envelope, properties, body);
+        String msg = new String(body, StandardCharsets.UTF_8);
+        logger.info(msg);
+    }
+}
